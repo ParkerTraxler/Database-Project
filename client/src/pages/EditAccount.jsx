be9b6af9eaf5a94175e05437b91fd92b122d4ab9
Alec Lindsey <madalec13@gmail.com>
2025-03-06 15:32:50 -0600

    This is a new branch made by Alec Lindsey. In it is the Review, Account Details, Edit Account, Log in, and Sign up pages and a visual change to the Navbar.

---
+++ client/src/pages/EditAccount.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Account.css'
+
+const EditAccount = () => {
+    console.log("EditAccount")
+    
+
+    return(
+        <div className="AccountDetails">
+            <h1>Edit Account</h1>
+            <div className="detailsBox">
+                <div className="detail">
+                    <strong>First Name:</strong>
+                    <input type="text" maxLength="28" name="firstName"></input>
+                </div>
+                <div className="detail">
+                    <strong>Last Name:</strong>
+                    <input type="text" maxLength="28" name="lastName"></input>
+                </div>
+                <div className="detail">
+                    <strong>Address:</strong>
+                    <input type="address" maxLength="70" name="address"></input>
+                </div>
+                <div className="detail">
+                    <strong>Date of Birth:</strong>
+                    <input type="date" name="birthDate"></input>
+                </div>
+                <div className="detail">
+                    <strong>Gender:  </strong>
+                    <select>
+                        <option value="">---Choose an option---</option>
+                        <option value="1">Male</option>
+                        <option value="2">Female</option>
+                        <option value="3">Other</option>
+                    </select>
+                </div>
+            </div>
+            <div>
+                <button className="saveButton">Save Changes</button>
+            </div>
+        </div>
+    )
+}
+
+export default EditAccount
--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,12 +1,68 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios' //api calls
+import './LogIn.css' // SignUp.jsx also uses LogIn.css
+
 
 const LogIn = () => {
     console.log("LogIn")
+    const invalidChars = /[+=*\/<>"'|~`()^_{}[\]?]/;
+
+    const [login, setLogin] = useState({ //login json inialized to be empty
+        email:"",
+        password:"",
+    })
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleChange = (e) =>{ // given target to given value
+        setLogin(prev=>({...prev, [e.target.name]: e.target.value}))
+        console.log(login)
+    }
+
+    const handleClick = async (e) => {
+        console.log(login)
+        if (invalidChars.test(login.email)) {
+            setErrorMessage("Email has invalid characters.");
+            return;
+        }
+        else if (invalidChars.test(login.password)) {
+            setErrorMessage("Password has invalid characters.");
+            return;
+        }
+        else{
+            e.preventDefault()  //prevents page refresh on button click
+            try{
+                await axios.post("http://localhost:3000/log-in", login)
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+    }
 
     return(
-        <div>
-            Log In
+        <div className="container">
+            <div className="login-box">
+                
+                <h1>Log In</h1>
+                <div className="error">{errorMessage}</div>
+                <div className="input-group">
+                    Email
+                    <input type="email" placeholder="Enter your email"  maxLength="30" onChange={handleChange} name="email"/>
+                </div>
+
+                <div className="input-group">
+                    Password
+                    <input type="password" placeholder="Enter your password" maxLength="16" onChange={handleChange} name="password"/>
+                </div>
+                <div className="no-account">
+                    Don't have an account? <a href="/sign-up">Sign Up</a>
+                </div>
+                <button onClick={handleClick}>Log In</button> 
+            </div>
         </div>
+        
     )
 }
 
