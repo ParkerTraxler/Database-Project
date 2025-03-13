ad847a12cabe7520e03b658fd33b8e5e7d37fcdb
ParkerTraxler <parker_traxler@hotmail.com>
2025-03-13 11:16:35 -0700

    moved CORS middleware to server.js

--- server/routes.js
+++ server/routes.js
@@ -1,26 +1,12 @@
 // routes.js
 const http = require('http');
-const cors = require('cors'); // CORS module
 const loginController = require('./controllers');
 
-const corsMiddleware = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204);
-        res.end();
-        return;
-    }
-};
+
 
 const handleRoutes = (req, res) => {
-    corsMiddleware(req, res);  // Enable CORS for all routes
 
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/log-in' && method === 'POST') {
+    if (req.url === '/log-in' && req.method === 'POST') {
         loginController.loginUser(req, res);
     } else {
         if (!res.headersSent) {
--- server/server.js
+++ server/server.js
@@ -2,7 +2,23 @@ const http = require('http');
 const handleRoutes = require('./routes');
 const PORT = 3001;
 
-const server = http.createServer(handleRoutes);
+// CORS middleware
+const corsMiddleware = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+};
+
+const server = http.createServer((req, res) => {
+    corsMiddleware(req, res);
+    handleRoutes(req, res);
+})
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
