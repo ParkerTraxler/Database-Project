bd628095abbd2304e6cfbdef8522c187db06d802
SiamBhuiyan5 <siambhuiyan458@gmail.com>
2025-03-14 21:12:57 -0500

    k

--- client/src/pages/Reviews.jsx
+++ client/src/pages/Reviews.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { FaStar, FaStarHalf, FaStarHalfAlt } from "react-icons/fa";
+import { useState } from 'react'
+import './Reviews.css'
 
 const Reviews = () => {
     console.log("Reviews")
-
+    
+    const [review, setReview] = useState({
+        rating: 0,
+        description: "",
+        reviewDate: null,
+    })
+    
+    const stars = Array(5).fill(0)
+    
+      
     return(
         <div>
             Reviews
+            
+            <div className='starReview'>
+                {stars.map((_, index) => {
+                    let rating = 3.5;
+                    return (
+                        <div key={index} >
+                            <FaStarHalf size={24} color={(rating) > index ? 'gold' : 'grey'} />
+                            <FaStarHalf className="mirroredStar" size={24} color={(rating) > index + 0.5 ? 'gold' : 'grey'}/>
+                        </div>   
+                    );
+                })}
+            </div>
+            <p></p>
+            
+            <a href = "/write-review">Review Us!</a>
         </div>
     )
 }
