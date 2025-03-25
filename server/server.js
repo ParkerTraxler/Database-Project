ddd43266f98df070856970b9c6ac0c4dc2b80c1f
Parker Traxler <parker_traxler@hotmail.com>
2025-03-25 18:33:54 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const manageReviewsRoutes = require('./routes/manageReviewsRoute');
 const manageSalesRoutes = require('./routes/manageSalesRoute')
 const manageProfileRoutes = require('./routes/manageUserAccRoutes')
 // Port
-const PORT = 3002;
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
 
